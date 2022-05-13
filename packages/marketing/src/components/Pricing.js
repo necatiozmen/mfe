d76a88180ca07cd6f4ce2bba7261ee40e1963251
necatiozmen <necatiozmn@gmail.com>
2022-05-13 13:53:09 +0300

    Mock update

--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -18,7 +18,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+        Your Websitex
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
